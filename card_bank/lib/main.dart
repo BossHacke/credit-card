73adcf68e3ba4d1995d7bd800e6b81a4326a5685
BossHacke <vonguyenphihung123@gmail.com>
2024-06-11 10:13:03 +0700

    update

--- card_bank/lib/main.dart
+++ card_bank/lib/main.dart
@@ -1,125 +1,295 @@
+import 'package:card_bank/generator/assets.gen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_credit_card/flutter_credit_card.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String cardNumber = '';
+  String expiryDate = '';
+  String cardHolderName = '';
+  String cvvCode = '';
+  bool isCvvFocused = false;
+  OutlineInputBorder? border;
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  TextEditingController bankName = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    bankName = TextEditingController(text: "");
+    border = OutlineInputBorder(
+      borderSide: BorderSide(
+        color: Colors.grey,
+        width: 2,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      resizeToAvoidBottomInset: true,
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.black12,
+        title: Center(
+          child: Text(
+            'Credit Card',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: Assets.images.bg.provider(),
+            fit: BoxFit.fill,
+          ),
+        ),
+        child: SafeArea(
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                height: 50,
+              ),
+              CreditCardWidget(
+                //spell:ignore glassmorphismConfig Glassmorphism
+                glassmorphismConfig: Glassmorphism.defaultConfig(),
+                enableFloatingCard: true,
+                floatingConfig: FloatingConfig(
+                  isGlareEnabled: true,
+                  isShadowEnabled: true,
+                  shadowConfig: FloatingShadowConfig(
+                    offset: Offset(10, 10),
+                    color: Colors.black87,
+                    blurRadius: 15,
+                  ),
+                ),
+                bankName: bankName.text,
+                backgroundImage: 'assets/images/card_bg.png',
+                cardNumber: cardNumber,
+                expiryDate: expiryDate,
+                cardHolderName: cardHolderName,
+                cvvCode: cvvCode,
+                frontCardBorder: Border.all(color: Colors.grey),
+                backCardBorder: Border.all(color: Colors.grey),
+                showBackView: isCvvFocused,
+                obscureCardNumber: true,
+                obscureCardCvv: true,
+                isHolderNameVisible: true,
+                cardBgColor: Colors.black,
+                isSwipeGestureEnabled: true,
+                onCreditCardWidgetChange: (CreditCardBrand creditCardBrand) {},
+                customCardTypeIcons: [
+                  CustomCardTypeIcon(
+                    cardType: CardType.mastercard,
+                    cardImage: Image(
+                      image: Assets.images.mastercard.provider(),
+                      height: 48,
+                      width: 48,
+                    ),
+                  ),
+                ],
+              ),
+              Expanded(
+                child: ListView(
+                  children: [
+                    Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(
+                              left: 17, right: 17, top: 20),
+                          child: TextFormField(
+                            controller: bankName,
+                            style: const TextStyle(color: Colors.white),
+                            onEditingComplete: (() => setState(() {})),
+                            decoration: InputDecoration(
+                              labelText: 'Bank Name',
+                              hintText: 'Name',
+                              hintStyle: const TextStyle(color: Colors.white),
+                              labelStyle: const TextStyle(color: Colors.white),
+                              focusedBorder: border,
+                              enabledBorder: border,
+                            ),
+                          ),
+                        ),
+                        CreditCardForm(
+                          formKey: formKey,
+                          // autovalidateMode: AutovalidateMode.onUnfocus,
+                          cvvValidationMessage: 'Please input a valid CVV',
+                          dateValidationMessage: 'Please input a valid date',
+                          numberValidationMessage:
+                              'Please input a valid number',
+                          obscureCvv: true,
+                          obscureNumber: true,
+                          cardNumber: cardNumber,
+                          cardHolderName: cardHolderName,
+                          expiryDate: expiryDate,
+                          cvvCode: cvvCode,
+                          isHolderNameVisible: true,
+                          isCardNumberVisible: true,
+                          isExpiryDateVisible: true,
+                          onCreditCardModelChange: onCreditCardModelChange,
+                          //spell:ignore autovalidateMode
+                          inputConfiguration: const InputConfiguration(
+                            cardNumberDecoration: InputDecoration(
+                              labelText: 'Card Number',
+                              hintText: 'XXXX XXXX XXXX XXXX',
+                              hintStyle: const TextStyle(color: Colors.white),
+                              labelStyle: const TextStyle(color: Colors.white),
+                              border: OutlineInputBorder(),
+                            ),
+                            expiryDateDecoration: InputDecoration(
+                              hintStyle: const TextStyle(color: Colors.white),
+                              labelStyle: const TextStyle(color: Colors.white),
+                              border: OutlineInputBorder(),
+                              labelText: 'Expired Date',
+                              hintText: 'XX/XX',
+                            ),
+                            cvvCodeDecoration: InputDecoration(
+                              hintStyle: const TextStyle(color: Colors.white),
+                              labelStyle: const TextStyle(color: Colors.white),
+                              border: OutlineInputBorder(),
+                              labelText: 'CVV',
+                              hintText: 'XXX',
+                            ),
+                            cardHolderDecoration: InputDecoration(
+                              hintStyle: const TextStyle(color: Colors.white),
+                              labelStyle: const TextStyle(color: Colors.white),
+                              border: OutlineInputBorder(),
+                              labelText: 'Card Holder',
+                            ),
+                            cardNumberTextStyle: TextStyle(
+                              fontSize: 16,
+                              color: Colors.white,
+                            ),
+                            cardHolderTextStyle: TextStyle(
+                              fontSize: 16,
+                              color: Colors.white,
+                            ),
+                            expiryDateTextStyle: TextStyle(
+                              fontSize: 16,
+                              color: Colors.white,
+                            ),
+                            cvvCodeTextStyle: TextStyle(
+                              fontSize: 16,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: _onValidate,
+                          child: Container(
+                            margin: const EdgeInsets.symmetric(
+                                horizontal: 16, vertical: 8),
+                            decoration: BoxDecoration(
+                              gradient: const LinearGradient(
+                                colors: <Color>[
+                                  Colors.purple,
+                                  Colors.amber,
+                                  Colors.blue
+                                ],
+                                begin: Alignment(-1, -4),
+                                end: Alignment(1, 4),
+                              ),
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            padding: const EdgeInsets.symmetric(vertical: 15),
+                            width: double.infinity,
+                            alignment: Alignment.center,
+                            child: const Text(
+                              'Success',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontFamily: 'halter',
+                                fontSize: 14,
+                                package: 'flutter_credit_card',
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  void _onValidate() {
+    if (formKey.currentState!.validate()) {
+      //spell:ignore thêm thành công
+      _showValidDialog(
+          context, "Hợp lệ", "Thẻ của bạn hợp lệ thêm thành công !!!");
+    } else {
+      //spell:ignore Không
+      _showValidDialog(context, "Không hợp lệ", "Thử lại !!!");
+    }
+  }
 
-  void _incrementCounter() {
+  void onCreditCardModelChange(CreditCardModel? creditCardModel) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      cardNumber = creditCardModel!.cardNumber;
+      expiryDate = creditCardModel.expiryDate;
+      cardHolderName = creditCardModel.cardHolderName;
+      cvvCode = creditCardModel.cvvCode;
+      isCvvFocused = creditCardModel.isCvvFocused;
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+  _showValidDialog(
+    BuildContext context,
+    String title,
+    String content,
+  ) {
+    showDialog<AlertDialog>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: Colors.white,
+          title: Text(title),
+          content: Text(content),
+          actions: [
+            Builder(
+              builder: (_) => ElevatedButton(
+                onPressed: () => Navigator.pop(context),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.black,
+                ),
+                child: Text(
+                  'OK',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
             ),
           ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        );
+      },
     );
   }
 }
